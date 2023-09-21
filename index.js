2dd09d0a4ee589af1df0fc88694f9b5487233eaa
nbali12 <BaliNishant@JohnDeere.com>
2023-09-21 16:09:09 -0400

    adding order.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
+const { itemsToSell, orders } = require('./orders');
 const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express.js!');
+  const jsonResponse = {
+    message: 'Hello, Express.js!',
+    date: new Date()
+  };
+
+  res.json(jsonResponse);
+});
+
+app.get('/items-to-sell', (req, res) => {
+  res.json(itemsToSell);
+});
+
+app.get('/orders', (req, res) => {
+  res.json(orders);
 });
 
 app.listen(port, () => {
---
+++ orders.js
@@ -0,0 +1,98 @@
+const itemsToSell = {
+  'ValentinesBox': {
+    contains: [
+      'Red Roses Bouquet',
+      'Box of chocolates',
+      'Love card',
+      'Womens perfume'
+    ]
+  },
+  'BirthdayBox': {
+    contains: [
+      'Birthday cupcake',
+      '$100 Visa Gift Card',
+      'Birthday card'
+    ]
+  },
+  'ClientGiftBox': {
+    contains: [
+      'Bottle of wine',
+      'Fruit basket',
+      'Pen'
+    ]
+  }
+};
+
+const orders = [
+  {
+    orderNumber: 1,
+    orderDate: 'September 15th, 2023',
+    lineItems: [
+      {
+        'BirthdayBox': {
+          contains: [
+            'Birthday cupcake',
+            '$100 Visa Gift Card',
+            'Birthday card'
+          ]
+          },
+        'ValentinesBox': {
+          contains: [
+            'Red Roses Bouquet',
+            'Box of chocolates',
+            'Love card',
+            'Womens perfume'
+          ]
+        }
+      },
+    ],
+    shipTo: {
+      name: 'John Smith',
+      address: '100 Dundas Street East'
+    }
+  },
+  {
+    orderNumber: 2,
+    orderDate: 'September 15th, 2023',
+    lineItems: [
+      {
+        'ValentinesBox': {
+          contains: [
+            'Red Roses Bouquet',
+            'Box of chocolates',
+            'Love card',
+            'Womens perfume'
+          ]
+        }
+      }
+    ],
+    shipTo: {
+      name: 'Rob Smith',
+      address: '100 Queen Street East'
+    }
+  },
+  {
+    orderNumber: 3,
+    orderDate: 'September 15th, 2023',
+    lineItems: [
+      {
+        'BirthdayBox': {
+          contains: [
+            'Birthday cupcake',
+            '$100 Visa Gift Card',
+            'Birthday card'
+          ]
+        }
+      },
+    ],
+    shipTo: {
+      name: 'Dan Smith',
+      address: '100 King Street East'
+    }
+  }
+];
+
+module.exports = {
+  itemsToSell,
+  orders
+};
